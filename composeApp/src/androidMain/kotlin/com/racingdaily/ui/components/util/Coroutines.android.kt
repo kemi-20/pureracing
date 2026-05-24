7eb0dc5808ac4d60d9977052e320e43c7bf24c04
kemi-20 <yuminghao805@gmail.com>
2026-05-24 15:30:51 +0800

    Fix awaitFrame return type mismatch on Android

--- composeApp/src/androidMain/kotlin/com/racingdaily/ui/components/util/Coroutines.android.kt
+++ composeApp/src/androidMain/kotlin/com/racingdaily/ui/components/util/Coroutines.android.kt
@@ -1,5 +1,5 @@
 package com.racingdaily.ui.components.util
 
-import kotlinx.coroutines.android.awaitFrame
-
-actual suspend fun awaitFrame(): Unit = kotlinx.coroutines.android.awaitFrame()
+actual suspend fun awaitFrame() {
+    kotlinx.coroutines.android.awaitFrame()
+}

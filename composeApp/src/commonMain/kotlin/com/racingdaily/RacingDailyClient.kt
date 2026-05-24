c5d4e3138677291ec24bb9f9920c38abb73d1786
kemi-20 <yuminghao805@gmail.com>
2026-05-24 16:27:19 +0800

    Fix image loading: add news.romielf.com Referer header for Coil to bypass CDN restriction

--- composeApp/src/commonMain/kotlin/com/racingdaily/RacingDailyClient.kt
+++ composeApp/src/commonMain/kotlin/com/racingdaily/RacingDailyClient.kt
@@ -1,13 +1,30 @@
 package com.racingdaily
 
 import androidx.compose.runtime.Composable
+import coil3.ImageLoader
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.network.ktor3.KtorNetworkFetcherFactory
 import com.racingdaily.data.remote.ApiService
 import com.racingdaily.di.appModule
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
 import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
 
 @Composable
 fun RacingDailyClient() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .components {
+                add(KtorNetworkFetcherFactory(httpClient = HttpClient {
+                    defaultRequest {
+                        header("Referer", "https://news.romielf.com")
+                    }
+                }))
+            }
+            .build()
+    }
     KoinApplication(application = { modules(appModule) }) {
         val api = koinInject<ApiService>()
         App(api)

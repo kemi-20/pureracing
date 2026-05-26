2d6e188b7dddc85cdd260a7a94e563a3bc319653
kemi-20 <yuminghao805@gmail.com>
2026-05-26 22:07:19 +0800

    Skip release lint vital in CI

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,6 +91,10 @@ android {
         }
     }
 
+    lint {
+        checkReleaseBuilds = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
